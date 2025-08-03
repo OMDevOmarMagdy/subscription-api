1da2d30a114781e3bb03518e6dcc7d3c78350d08
Omar Magdy <om8622145@gmail.com>
2025-08-03 19:06:45 +0300

    .

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_CONNECTION);
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("MongoDB error:", error.message);
-    process.exit(1);
-  }
+const connectDB = () => {
+  const db = process.env.DB_CONNECTION;
+  return mongoose.connect(db).then(() => {
+    console.log("DB Connect Successfully");
+  });
 };
 
 module.exports = connectDB;
